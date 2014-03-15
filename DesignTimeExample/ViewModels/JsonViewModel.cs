55fde03372fdb95b6f205bcbdd05ec4b575a4f08
Martin Suchan <martin.s@mineus.net>
2014-03-15 19:49:59 +0100

    Added sample, how to use for design time data source Json file.

---
+++ DesignTimeExample/ViewModels/JsonViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignTimeExample.ViewModels
+{
+	/// <summary>
+	/// Reference object for Json design time data source is quite sensitive:
+	/// target properties must have public getter and setter and property names must match those in json file.
+	/// </summary>
+	public class JsonViewModel
+	{
+		public string Data { get; set; }
+
+		public string Color { get; set; }
+	}
+}
