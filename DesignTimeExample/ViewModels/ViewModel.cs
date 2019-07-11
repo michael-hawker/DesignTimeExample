e74900707e8c04a90f6d2dfbf5a89b7cb133d96e
michael-hawker <michael.hawker@outlook.com>
2019-07-11 13:24:36 -0700

    Update Project to UWP App
    TODO: Understand Gaps

--- DesignTimeExample/ViewModels/ViewModel.cs
+++ DesignTimeExample/ViewModels/ViewModel.cs
@@ -4,8 +4,8 @@ namespace DesignTimeExample.ViewModels
 {
     public class ViewModel : IViewModel
     {
-        public string Data { get { return "Real data"; } }
-        public string Color { get { return "#5EFF00"; } }
+        public string Data { get { return "Real data"; } set { } }
+        public string Color { get { return "#5EFF00"; } set { } }
     }
 
     public class ViewModel2
